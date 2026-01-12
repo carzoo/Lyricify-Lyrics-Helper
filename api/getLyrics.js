bea3204f5dd1a73d562a1805c4e50d3c6735a02f
carzoo <47290921+carzoo@users.noreply.github.com>
2026-01-12 19:41:28 +0900

    Update getLyrics.js

--- api/getLyrics.js
+++ api/getLyrics.js
@@ -1,13 +1,5 @@
 export default function handler(req, res) {
-  const { song, artist } = req.query;
-
-  if (!song || !artist) {
-    return res.status(400).json({ error: "song and artist required" });
-  }
-
   res.status(200).json({
-    song,
-    artist,
-    lyrics_lrc: `[00:01.00]Song: ${song}\n[00:02.00]Artist: ${artist}\n[00:03.00]Lyrics not implemented yet...`
+    query: req.query
   });
 }
